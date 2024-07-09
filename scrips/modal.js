2790ba721f898210a27eaeb0fd1683058aacb965
gretawix <gretav@wix.com>
2024-07-09 16:00:29 +0300

    modal inputs

--- scrips/modal.js
+++ scrips/modal.js
@@ -10,9 +10,22 @@ const modalfunctionality = () => {
     selectionBtns.forEach((item) => {
         item.addEventListener("click", (event) => {
             const settingsDiv = item.parentElement.querySelector(".settings");
-            console.log(settingsDiv);
             settingsDiv.style.display = "block";
             item.style.display = "none";
+            const inputs = settingsDiv.querySelectorAll("input");
+            if (!Array.from(inputs).some((item) => item === document.activeElement)) {
+                settingsDiv.querySelector("input").focus();
+            }
+        });
+    });
+
+    const inputActivationBtns = document.querySelectorAll(".event-settings-selection .clickable");
+    inputActivationBtns.forEach((btn) => {
+        btn.addEventListener("click", (event) => {
+            const inputDataId = btn.getAttribute("data-clickable-id");
+            const inputParent = btn.closest(".selection-content");
+            const targetInput = inputParent.querySelector(`[data-clickable-input="${inputDataId}"]`);
+            targetInput.focus();
         });
     });
 
