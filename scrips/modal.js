66df9877b4ceb6241b4c41329cd6d80dfdbfa59d
gretawix <gretav@wix.com>
2024-07-09 17:19:58 +0300

    add more fields to modal

--- scrips/modal.js
+++ scrips/modal.js
@@ -9,23 +9,10 @@ const modalfunctionality = () => {
     const selectionBtns = document.querySelectorAll(".to-select");
     selectionBtns.forEach((item) => {
         item.addEventListener("click", (event) => {
-            const settingsDiv = item.parentElement.querySelector(".settings");
-            settingsDiv.style.display = "block";
-            item.style.display = "none";
-            const inputs = settingsDiv.querySelectorAll("input");
-            if (!Array.from(inputs).some((item) => item === document.activeElement)) {
-                settingsDiv.querySelector("input").focus();
-            }
-        });
-    });
-
-    const inputActivationBtns = document.querySelectorAll(".event-settings-selection .clickable");
-    inputActivationBtns.forEach((btn) => {
-        btn.addEventListener("click", (event) => {
-            const inputDataId = btn.getAttribute("data-clickable-id");
-            const inputParent = btn.closest(".selection-content");
-            const targetInput = inputParent.querySelector(`[data-clickable-input="${inputDataId}"]`);
-            targetInput.focus();
+            const parent = item.closest(".single-setting-section");
+            const settingsDiv = parent.querySelector(".full-settings");
+            settingsDiv.style.display = "flex";
+            parent.querySelector(".preview-settings").style.display = "none";
         });
     });
 
