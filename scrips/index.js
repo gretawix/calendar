f2801ab0f77f541dc692d19216fdc0ea2cea5fe1
gretawix <gretav@wix.com>
2024-07-09 11:33:15 +0300

    add scroll functionality to grid hours and days

---
+++ scrips/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.querySelector("#days-hours-grid");
+    const daysRow = document.querySelector("#days-row");
+    const hoursCol = document.querySelector("#hours-col");
+    grid.addEventListener("scroll", (event) => {
+        hoursCol.scrollTop = grid.scrollTop;
+        daysRow.scrollLeft = grid.scrollLeft;
+    });
+});
