7e416ecc81e479742ce5b71500c108586d1131ee
gretawix <gretav@wix.com>
2024-08-09 10:28:14 +0300

    remove dist folder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import initWeekView from "./weekView.js";
-import { initModal } from "./modal.js";
-import { displayAllSavedEvents } from "./eventsData.js";
-document.addEventListener("DOMContentLoaded", () => {
-    initWeekView();
-    initModal();
-    displayAllSavedEvents();
-});
-//# sourceMappingURL=index.js.map
